0da223f82ae5a72683058b8dd4dad3390ed95949
Shea Murphy <sm967@cornell.edu>
2018-03-26 18:08:32 -0400

    comments in server.mli
    title

--- server.mli
+++ server.mli
@@ -4,7 +4,14 @@ open Tags
 open Post
 open Comment
 
+(* [users] returns a list of all users. *)
 val users : user list
+
+(* [posts] returns a list of all posts. *)
 val posts : post list
+
+(* [comments] returns a list of all comments. *)
 val comments : comment list
+
+(* [tags] returns a list of all tags. *)
 val tags : tag list
