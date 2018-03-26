6ce6247f2c63a34a67fc867db3939be9112b9256
arzumm <37027236+arzumm@users.noreply.github.com>
2018-03-26 18:32:05 -0400

    changes to user.mli

--- user.mli
+++ user.mli
@@ -1,3 +1,19 @@
 open Opium.Std
 
-val username : string
+
+module type User = sig
+
+  type t = {
+    username : string;
+    mutable posts : post list;
+    mutable exposed_users : string list;
+  }
+
+  val score_threshold: int
+
+  val score_calculator: string -> string -> int
+
+  val interaction_score : ((string*string),int) Hashtbl.t
+
+end
+
