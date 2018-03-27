26368c92c2a11c7a4ed28e4f0c608aa0118fbab9
Chris Umeki <ctu3@cornell.edu>
2018-03-27 00:32:53 -0400

    Organize Entry Interface

--- entry.ml
+++ entry.ml
@@ -1,1 +1,46 @@
-open Opium.Std
+
+module type Entry = sig
+  type t
+  val make_post : string -> bool -> string -> string -> t
+  val add_reply : string -> string -> t -> t
+end
+
+module Comment : Entry = struct
+
+  type t = {
+    id: int;
+    score: int;
+    text: string;
+    user: string;
+    mutable children: t list;
+    parent_id: string;
+  }
+
+  let make_post a b c d =
+    failwith "Unimplemented"
+
+  let add_reply a b c =
+    failwith "Unimplemented"
+
+end
+
+module Post : Entry = struct
+
+  type t = {
+      id: int;
+      score: int;
+      title: string;
+      text: string;
+      is_url: bool;
+      user: string;
+      mutable children: t list;
+      tag: string;
+  }
+
+  let make_post a b c d =
+    failwith "Unimplemented"
+
+  let add_reply a b c =
+    failwith "Unimplemented"
+
+end
--- entry.mli
+++ entry.mli
@@ -1,38 +1,20 @@
 
+module type Entry = sig
+
+  type t
+
+  (* [make_post text cont tag user] creates posts
+  *)
+  val make_post : string -> bool -> string -> string -> t
+
+  (* [add_reply text user par]
+  *)
+  val add_reply : string -> string -> t -> t
+
+end
+
+module Comment : Entry
+
+module Post : Entry
 
 
-type medium = Text | Url | Image
-
-type comment = {
-  id: int;
-  score: int;
-  text: string;
-  content: medium;
-  user: string;
-  mutable children: comment list;
-  parent: string;
-}
-
-type post = {
-  id: int;
-  score: int;
-  text: string;
-  content: medium;
-  tag: string;
-  user: string;
-  mutable children: comment list;
-}
-
-type entry = Post of post | Comment of comment
-
-(* [generate ()] is a stream that always returns a unique int
-*)
-val generate : unit -> int
-
-(* [make_post text cont tag usr] creates posts
-*)
-val make_post : string -> medium -> string -> string -> post
-
-(* [add_reply scorepar ]
-*)
-(* val add_reply :  *)
