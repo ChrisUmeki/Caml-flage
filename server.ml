934a0885c07bfdda3b412bb317efc61e418f0a2a
Chris Umeki <ctu3@cornell.edu>
2018-04-14 18:14:26 -0400

    Store GET request string to record

--- server.ml
+++ server.ml
@@ -1,8 +1,27 @@
 open Opium.Std
 
-let hello = get "/" (fun _ -> `String "Hello World" |> respond')
+type state = {
+    mutable s : string; 
+}
+
+let listen st = get "/:id/:text" begin fun req ->
+    st.s <- (param req "text"); 
+    `String ( param req "id" )|> respond'
+end
+
+let front st = get "/" begin fun req ->
+    `String ("Front page! Our state is: " ^ st.s) |> respond'
+end
+
+let not_found = get "/*" begin fun req ->
+    `String ("Not found") |> respond'
+end
+
+let my_state : state = {s = "old state"}
 
 let () = App.empty
-         |> hello
+         |> listen my_state
+         |> front my_state
+         |> not_found 
          |> App.run_command
          |> ignore
