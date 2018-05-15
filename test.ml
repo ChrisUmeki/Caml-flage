403fbb1c9435127ee6379941cf0517d7f9158a30
arzumm <am2692@cornell.edu>
2018-05-14 21:17:22 -0400

    more testing

--- test.ml
+++ test.ml
@@ -11,7 +11,6 @@ let post2 = post_from_params 431 51 "test" "this is a test" false None "coolguy4
 let post3 = post_from_params 431 49 "test" "this is a test" false None "coolguy42" [] "testtag" 1526275325.
 let post4 = post_from_params 431 50 "test" "this is a test" false None "coolguy42" [comment1] "testtag" 1526275325.
 let post5 = post_from_params 431 (-10) "test" "this is a test" false None "coolguy42" [] "testtag" 1526275325.
-let comment2 = comment_from_params 1000 (-7) "wow i like your test" "coolguy43" [] 431
 let comment3 = comment_from_params 1000 (-9) "wow i like your test" "coolguy43" [] 431
 let comment4 = comment_from_params 1001 (4) "i agree nice test bro" "coolguy44" [] 1000
 let comment5 = comment_from_params 1000 (-8) "wow i like your test" "coolguy43" [comment4] 431
@@ -22,6 +21,9 @@ let unit = add_post tag2 post3
 let unit1 = Post.up_camel post4
 let unit2 = Post.down_camel post5 
 let unit3 = Post.add_reply post2 comment2 
+let unit4 = Comment.up_camel comment4 
+let unit5 = Comment.down_camel comment5 
+let unit6 = Comment.add_reply comment5 comment3
 
 
 let tests =
@@ -30,6 +32,7 @@ let tests =
     "post1 score" >:: (fun _ -> assert_equal 50 (Post.get_score post1));
     "post1 title" >:: (fun _ -> assert_equal "test" (Post.get_title post1));
     "post1 text" >:: (fun _ -> assert_equal "this is a test" (Post.get_text post1));
+    "post2 text" >:: (fun _ -> assert_equal "this is a test" (Post.get_text post2));
     "post1 tag" >:: (fun _ -> assert_equal "testtag" (Post.get_tag post1));
     "post1 score up-cameled" >:: (fun _ -> assert_equal 51 (Post.get_score post4));
     "post1 score down-cameled" >:: (fun _ -> assert_equal (-11) (Post.get_score post5));
@@ -39,13 +42,14 @@ let tests =
     "post5 neg hot score" >:: (fun _ -> assert_equal (-207) (Post.get_hot_score post5));
     "comment1 id" >:: (fun _ -> assert_equal 1000 (Comment.get_id comment1));
     "comment1 neg score" >:: (fun _ -> assert_equal (-8) (Comment.get_score comment1));
+    "comment2 text" >:: (fun _ -> assert_equal "this is the greatest comment on earth" (Comment.get_text comment2));
     "comment1 text" >:: (fun _ -> assert_equal "wow i like your test" (Comment.get_text comment1));
     "comment1 children" >:: (fun _ -> assert_equal [] (Comment.get_children comment1));
     "comment1 par id" >:: (fun _ -> assert_equal 431 (Comment.get_par comment1));
-    (* "comment1 score up-cameled" >:: (fun _ -> assert_equal comment2 (Comment.up_camel comment1));
-    "comment1 score down-cameled" >:: (fun _ -> assert_equal comment3 (Comment.down_camel comment1));
-    "comment1 add reply" >:: (fun _ -> assert_equal comment5 (Comment.add_reply comment1 comment1)); *)
-    "comment5 children" >:: (fun _ -> assert_equal [comment4] (Comment.get_children comment5));
+    "comment2 par id" >:: (fun _ -> assert_equal 431 (Comment.get_par comment2));
+    "comment1 score up-cameled" >:: (fun _ -> assert_equal 5 (Comment.get_score comment4));
+    "comment1 score down-cameled" >:: (fun _ -> assert_equal (-9) (Comment.get_score comment5));
+    "comment1 add reply" >:: (fun _ -> assert_equal [comment3; comment4] (Comment.get_children comment5));
     "tag01" >:: (fun _ -> assert_equal "hi" (Tag.tag_name tag1));
     "tag02" >:: (fun _ -> assert_equal [] (Tag.posts_list tag3));
     "tag03" >:: (fun _ -> assert_equal [post1;post2] (Tag.posts_list tag1));
